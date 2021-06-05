2fa1cc91b4e6bd1fff9011d3ee682391972d9b1b
Uros Blagojevic <71049196+blagojevic-uros@users.noreply.github.com>
2021-06-06 00:20:52 +0200

    Initial Commit

---
+++ HCI_Projekat/HCI_Projekat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using HCI_Projekat.Core;
+using System;
+
+namespace HCI_Projekat.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand LoginViewCommand { get; set; }
+        public RelayCommand RegisterViewCommand { get; set; }
+        public RelayCommand ExitSystemCommand { get; set; }
+
+        public LoginViewModel LoginVM { get; set; }
+        public RegisterViewModel RegisterVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            LoginVM = new LoginViewModel();
+            RegisterVM = new RegisterViewModel();
+
+            CurrentView = LoginVM;
+
+            LoginViewCommand = new RelayCommand(o => {
+                CurrentView = LoginVM;
+            });
+            RegisterViewCommand = new RelayCommand(o => {
+                CurrentView = RegisterVM;
+            });
+            ExitSystemCommand = new RelayCommand(o => {
+                Environment.Exit(0);
+            });
+        }
+    }
+}
