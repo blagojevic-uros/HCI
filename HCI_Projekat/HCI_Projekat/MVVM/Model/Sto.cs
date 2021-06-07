ed6402a3e36511681fa56f1cd8c18dcf10091667
AcaJo <crazyaca45@gmail.com>
2021-06-07 22:12:53 +0200

    model

---
+++ HCI_Projekat/HCI_Projekat/MVVM/Model/Sto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_Projekat.MVVM.Model
+{
+    class Sto
+    {
+        private int _br_stola;
+        private int _br_mesta;
+        private List<string> _imena;
+        public Sto(int br_stola, int br_mesta, List<string> imena)
+        {
+            _br_stola = br_stola;
+            _br_mesta = br_mesta;
+            _imena = imena;
+        }
+    }
+}
