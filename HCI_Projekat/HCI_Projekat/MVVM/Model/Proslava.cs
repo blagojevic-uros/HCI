9bf621c3c19e76358917f0fd81a0fa307bae0fc5
VukoticMarko <vukoticmarko99@Gmail.com>
2021-06-07 20:06:24 +0200

    model
    model

---
+++ HCI_Projekat/HCI_Projekat/MVVM/Model/Proslava.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_Projekat.MVVM.Model
+{
+    class Proslava
+    {
+        private Korisnik _klijent { get; set; }
+        private Korisnik _organizator { get; set; }//nije u konst
+        private Saradnik _saradnik { get; set; }
+        private string _tema { get; set; }
+        private DateTime _datum { get; set; }
+        private double _budzet { get; set; }
+        private int _kapacitet { get; set; }
+        private string _komentar { get; set; }
+        private List<Sto> _stolovi { get; set; }//nije u konst
+
+
+        public Proslava(Korisnik klijent, Saradnik saradnik, string tema, DateTime datum, double budzet, int kapacitet, string komentar)
+        {
+            _klijent = klijent;
+            _saradnik = saradnik;
+            _tema = tema;
+            _datum = datum;
+            _budzet = budzet;
+            _kapacitet = kapacitet;
+            _komentar = komentar;
+        }
+    }
+}
---
+++ HCI_Projekat/HCI_Projekat/MVVM/Model/Saradnik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_Projekat.MVVM.Model
+{
+    class Saradnik
+    {
+        private int _pib { get; set; }
+        private string _ime;
+        private string _adresa;
+        private int _kapacitet;
+        private int _br_stolova;
+        private string _slika;
+
+        public Saradnik(int pib, string ime, string adresa, int kapacitet, int br_stolova, string slika)
+        {
+            _pib = pib;
+            _ime = ime;
+            _adresa = adresa;
+            _kapacitet = kapacitet;
+            _br_stolova = br_stolova;
+            _slika = slika;
+        }
+
+        public Saradnik() { }
+    }
+}
